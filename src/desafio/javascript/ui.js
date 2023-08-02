2dab8c135adb11083444d183ade74528c2c916da
Gustavo Monte <gmonte@tutanota.com>
2023-08-02 12:34:05 -0300

    feat(delete):add delete functionality

--- src/desafio/javascript/ui.js
+++ src/desafio/javascript/ui.js
@@ -8,19 +8,39 @@ const createElementWith = (elementTag, properties = {}) => {
   return element
 }
 
+
+const addLocalStorage = (data) => localStorage.setItem(`id${data.id}`, data.id)
+const toDelete = (data, table, people) => () => {
+  addLocalStorage(data)
+  refreshPeopleListInUI(table, people)
+}
+
 const refreshPeopleListInUI = (table, people) => {
+  const idToDelete = { ...localStorage }
+
   const tBody = table.querySelector('tbody')
   const trsToRemove = tBody.querySelectorAll('tr')
+
+  const peopleProxy = [...people].filter(person => {
+    const idValuesOfDeletePeople = Object.values(idToDelete)
+    const convertIdToNumber = idValuesOfDeletePeople.map(id => Number(id))
+
+    if (!convertIdToNumber.includes(person.id)) {
+      return person
+    }
+  })
 
   trsToRemove.forEach((tr) => {
     tr.remove()
   })
 
-  people.forEach((person) => {
+  peopleProxy.forEach((person) => {
     const tr = createElementWith('tr')
     const tdButton = createElementWith('td')
     const deleteButton = createElementWith('button', { innerText: 'Deletar' })
     tdButton.append(deleteButton)
+
+    deleteButton.addEventListener('click', toDelete(person, table, people))
 
     tr.append(createElementWith('td', { innerText: person.id }))
     tr.append(createElementWith('td', { innerText: person.name }))
